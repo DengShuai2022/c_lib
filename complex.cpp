ec1ea9fa53ec0e8187adb08369d5fb6b8845e038
DengShuai2022 <113286747+DengShuai2022@users.noreply.github.com>
2023-11-30 15:57:51 +0800

    Add files via upload

---
+++ complex.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+
+int main(){
+
+	cout<<"jel"<<endl;
+}
+
+
+/*class complex{
+
+	float real;
+
+};
+
+void operator <<(ostream &out,complex &obj){
+
+
+	out<<obj.real<<"+"<<obj.imaginary<<"i";
+}*/
